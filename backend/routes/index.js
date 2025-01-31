70d1c233e965a85c08aa94d92cb7fdb2165fae53
JenW79 <jkwill646@gmail.com>
2025-01-30 19:56:34 -0600

    finalized CSRF setup

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,6 +2,9 @@
 const express = require('express');
 const router = express.Router();
 const apiRouter = require('./api')
+const path = require('path');
+
+
 
 
 // router.get('/hello/world', function(req, res) {
@@ -9,15 +12,41 @@ const apiRouter = require('./api')
 //   res.send('Hello World!');
 // });
 
-router.get("/api/csrf/restore", (req, res) => {
-  const csrfToken = req.csrfToken();
-  res.cookie("XSRF-TOKEN", csrfToken);
-  res.status(200).json({
-    'XSRF-Token': csrfToken
-  });
-});
+// router.get("/api/csrf/restore", (req, res) => {
+//   const csrfToken = req.csrfToken();
+//   res.cookie("XSRF-TOKEN", csrfToken);
+//   res.status(200).json({
+//     'XSRF-Token': csrfToken
+//   });
+// });
 
 router.use('/api', apiRouter);
 
+// In development: restore CSRF token
+if (process.env.NODE_ENV !== 'production') {
+  router.get('/api/csrf/restore', (req, res) => {
+    const csrfToken = req.csrfToken();
+    res.cookie('XSRF-TOKEN', csrfToken);
+    return res.json({ "XSRF-Token": csrfToken });
+  });
+}
+
+// In production: serve frontend and set CSRF token
+if (process.env.NODE_ENV === 'production') {
+  // Serve the frontend's index.html file at the root route
+  router.get('/', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.sendFile(path.resolve(__dirname, '../../frontend', 'dist', 'index.html'));
+  });
+
+  // Serve static frontend files
+  router.use(express.static(path.resolve(__dirname, '../../frontend', 'dist')));
+
+  // Serve the frontend's index.html file at all other routes NOT starting with /api
+  router.get(/^(?!\/?api).*/, (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.sendFile(path.resolve(__dirname, '../../frontend', 'dist', 'index.html'));
+  });
+}
 
 module.exports = router;

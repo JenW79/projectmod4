cbec66c71f491fb338c6aac0a0fcddd17a3a6a53
JenW79 <jkwill646@gmail.com>
2025-02-02 20:55:13 -0600

    added modals instead of pages

--- frontend/src/components/SignupFormModal/SignupFormModal.jsx
+++ frontend/src/components/SignupFormModal/SignupFormModal.jsx
@@ -1,14 +1,12 @@
 import { useState } from 'react';
 import * as sessionActions from '../../store/session';
-import { useDispatch, useSelector } from 'react-redux';
-import { Navigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { useModal } from '../../context/Modal';
 import './SignupForm.css';
 
 
-
-function SignupFormPage() {
+function SignupFormModal() {
   const dispatch = useDispatch();
-  const sessionUser = useSelector((state) => state.session.user);
   const [email, setEmail] = useState("");
   const [username, setUsername] = useState("");
   const [firstName, setFirstName] = useState("");
@@ -16,8 +14,9 @@ function SignupFormPage() {
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [errors, setErrors] = useState({});
+  const { closeModal } = useModal();
 
-  if (sessionUser) return <Navigate to="/" replace={true} />;
+  
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -31,7 +30,9 @@ function SignupFormPage() {
           lastName,
           password
         })
-      ).catch(async (res) => {
+      ) 
+      .then(closeModal)
+      .catch(async (res) => {
         const data = await res.json();
         if (data?.errors) {
           setErrors(data.errors);
@@ -120,4 +121,4 @@ function SignupFormPage() {
 }
    
 
-export default SignupFormPage;
+export default SignupFormModal;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,7 @@ import './index.css';
 import { Provider } from 'react-redux';
 import configureStore from '../src/store/store';
 import * as sessionActions from './store/session';
-
+import { Modal, ModalProvider } from './context/Modal';
 
 
 const store = configureStore();
@@ -22,8 +22,11 @@ if (process.env.NODE_ENV !== 'production') {
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ModalProvider>
+      <Provider store={store}>
+        <App />
+        <Modal />
+      </Provider>
+    </ModalProvider>
   </React.StrictMode>
 );

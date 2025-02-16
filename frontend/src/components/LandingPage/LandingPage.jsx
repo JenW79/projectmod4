29e5b86f8a8903fa0259500e0b07ddcbd0db35a2
JenW79 <jkwill646@gmail.com>
2025-02-16 15:19:38 -0600

    fixed rating logic

--- frontend/src/components/LandingPage/LandingPage.jsx
+++ frontend/src/components/LandingPage/LandingPage.jsx
@@ -3,14 +3,12 @@ import { useDispatch, useSelector } from "react-redux";
 import { fetchAllSpots } from "../../store/spots"; 
 import { useNavigate } from "react-router-dom";
 import "../LandingPage/LandingPage.css";
+import { calculateRating } from "../../utils/calculatingRating";
 
 function LandingPage() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const spots = useSelector((state) => state.spots.allSpots);
-
-  
-
 
   useEffect(() => {
     dispatch(fetchAllSpots());
@@ -22,51 +20,43 @@ function LandingPage() {
 
   return (
     <div className="spots-container">
-  {spots.map((spot) => (
-    <div 
-      key={spot.id} 
-      className="spot-tile" 
-      onClick={() => navigate(`/spots/${spot.id}`)} 
-      title={spot.name} 
-    >
-      <div className="spot-image-container">
-        <img 
-          src={spot.previewImage} 
-          alt={spot.name} 
-          className="spot-image" 
-        />
-      </div>
+      {spots.map((spot) => {
+       const { avgRating } = calculateRating(spot.Reviews || []);
 
-      <div className="spot-info-container">
-        <div className="spot-info">
-          <div className="spot-location">
-            {spot.city}, {spot.state}
+        return (
+          <div 
+            key={spot.id} 
+            className="spot-tile" 
+            onClick={() => navigate(`/spots/${spot.id}`)} 
+            title={spot.name} 
+          >
+            <div className="spot-image-container">
+              <img 
+                src={spot.previewImage} 
+                alt={spot.name} 
+                className="spot-image" 
+              />
+            </div>
+
+            <div className="spot-info-container">
+              <div className="spot-info">
+                <div className="spot-location">
+                  {spot.city}, {spot.state}
+                </div>
+                <div className="spot-price">
+                  ${spot.price} <span> night</span>
+                </div>
+              </div>
+
+              <div className="spot-rating">
+                <i className="fa-solid fa-star"></i> {avgRating} 
+              </div>
+            </div>
           </div>
-          <div className="spot-price">
-            ${spot.price} <span> night</span>
-          </div>
-        </div>
-        
-
-        <div className="spot-rating">
-  {spot.numReviews > 0 ? (
-    <>
-      <i className="fa-solid fa-star"></i> {spot.avgStarRating}
-    </>
-  ) : (
-    <>
-      <i className="fa-solid fa-star"></i> New
-    </>
-  )}
-</div>
-
-
-      </div>
+        );
+      })}
     </div>
-  ))}
-</div>
   );
 }
-
 
 export default LandingPage;
